2259f4b728c9fb28563a5d004619e3fd5397cabd
Bedrockbreaker <coolmandude101@gmail.com>
2024-12-23 22:50:27 -0700

    Bundle FFmpeg into builds for planned future features

--- README.md
+++ README.md
@@ -7,11 +7,17 @@ Modding Utility for Party Project
 > As far as I'm aware, Party Project does not explictly define a license, making it All Rights Reserved by default.
 > Download your own (free) copy of Party Project yourself from their itch page.
 
+> [!NOTE]
+> The default build configuration and releases for Party Pooper bundle an LGPL-licensed build of FFmpeg.
+> You can either modify the build script to substitute a different version of FFmpeg, or build Party Pooper from source using the `--ffmpeg shared` option to use your system's version of ffmpeg instead.
+> Alternatively, you can disable FFmpeg support and related commands entirely by building with `--ffmpeg none`.
+> The license for the version of FFmpeg that's bundled by default can be found at https://github.com/FFmpeg/FFmpeg/blob/master/LICENSE.md
+
 ## Usage
 
 (`PartyPooper --help`)
 ```
-Usage: PartyPooper [options] <file> [destination]
+Usage: PartyPooper [options] [command]
 
 Modding Utility for Party Project
 
@@ -40,7 +46,23 @@ Then, run the build tool for the platform(s) you're building for.
 node --experimental-strip-types ./tools/build.ts linux-x64 win-x64
 ```
 
-Alternatively, use the npm script to build both `linux-x64` and `win-x64`.
-```bash
-npm run build
+There are a number of options for building:
+(`node --experimental-strip-types ./tools/build.ts --help`)
 ```
+Usage: build [options] [targets...]
+
+builds the project
+
+Arguments:
+  targets                       Platform(s) to build for, e.g. 'linux-x64' or 'win-arm64' (default: linux-x64)
+
+Options:
+  -n, --node-version <version>  Node version to build for (default: v22.11.0)
+  -d, --debug                   Build in debug mode (default: false)
+  -f, --ffmpeg <mode>           Configure ffmpeg support. Static bundles ffmpeg into the binary. Shared uses the system ffmpeg. None disables ffmpeg support.
+                                (choices: "static", "shared", "none", default: "static")
+  -h, --help                    display help for command
+```
+
+> [!NOTE]
+> Some of the build defaults are dependant on the host platform and version of Node being used.
--- tools/build.ts
+++ tools/build.ts
@@ -1,132 +1,322 @@
 import { execSync } from "child_process";
-import { program } from "@commander-js/extra-typings";
-import { copyFileSync, createWriteStream, existsSync, mkdirSync, readFileSync, renameSync } from "fs";
+import { Argument, Option, program } from "@commander-js/extra-typings";
+import { copyFileSync, createWriteStream, existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import { homedir, tmpdir } from "os";
-import { join } from "path";
+import { basename, join } from "path";
 import { inject } from "postject";
 import { pipeline } from "stream/promises";
-import { extract } from "tar";
+import { extract as ExtractTar } from "tar";
+import XZ from "xz-decompress";
 
 import { ExtractZip } from "../src/Zip.ts";
+import { Readable } from "stream";
 
-function build(isDebug: boolean) {
+export type FfmpegMode = "static" | "shared" | "none";
+
+const cache = join(homedir(), ".cache", "partypooper");
+
+class BuildTarget {
+	public os: "linux" | "win";
+	public arch: "arm64" | "x64";
+	public isWindows: boolean;
+
+	public constructor(target: string) {
+		const match = target.match(/^(linux|win)-?(arm64|x?64)$/);
+		if (match === null) {
+			throw new Error(`Invalid or unsupported target: ${target}`);
+		}
+
+		const [os, arch] = match.slice(1);
+	
+		this.os = os as typeof this.os;
+		this.arch = arch === "64" ? "x64" : arch as typeof this.arch;
+		this.isWindows = os === "win";
+	}
+
+	public asNodeString(): string {
+		return `${this.os}-${this.arch}`;
+	}
+
+	public asFfmpegString(): string {
+		const arch = this.arch === "x64" ? "64" : this.arch;
+		return `${this.os}${arch}`;
+	}
+}
+
+class NodeVersion {
+	public major: number;
+	public minor: number;
+	public patch: number;
+
+	public static compare(a: NodeVersion, b: NodeVersion): -1 | 0 | 1 {
+		if (a.major < b.major) return -1;
+		if (a.major > b.major) return 1;
+		if (a.minor < b.minor) return -1;
+		if (a.minor > b.minor) return 1;
+		if (a.patch < b.patch) return -1;
+		if (a.patch > b.patch) return 1;
+		return 0;
+	}
+
+	public constructor(version: string) {
+		const match = version.match(/^v?(\d+)\.(\d+)\.(\d+)$/);
+
+		if (match === null) {
+			throw new Error(`Invalid node version: ${version}`);
+		}
+
+		[this.major, this.minor, this.patch] = match.slice(1).map(Number);
+	}
+
+	public compare(version: NodeVersion): -1 | 0 | 1 {
+		return NodeVersion.compare(this, version);
+	}
+
+	public toString(): string {
+		return `v${this.major}.${this.minor}.${this.patch}`;
+	}
+}
+
+const minimumNodeVersion = new NodeVersion("20.6.0");
+
+function build(isDebug: boolean, ffmpegMode: FfmpegMode) {
 	console.log(`Building ${isDebug	? "Debug" : "Release"}`);
 
-	const rollupCommand = `npx rollup --config rollup.config.ts --configPlugin typescript --environment BUILD:${isDebug ? "debug" : "release"}`;
+	const rollupCommand = `npx rollup --config rollup.config.ts --configPlugin typescript --environment BUILD:${isDebug ? "debug" : "release"},FFMPEG:${ffmpegMode}`;
 	execSync(rollupCommand, { stdio: "inherit" });
 }
 
-function generateBlob() {
-	const [major, minor] = process.version.split(".").map(Number);
-	if (major < 20 || (major >= 20 && minor < 6)) {
-		program.error(`Unsupported node version: ${process.version}. The minimum supported version is 20.6.0`);
+function generateBlob(target: BuildTarget, ffmpegMode: FfmpegMode) {
+	if (minimumNodeVersion.compare(new NodeVersion(process.version)) > 0) {
+		throw new Error(`Incompatible node version: ${process.version}. Minimum required: ${minimumNodeVersion}`);
 	}
 
-	const blobCommand = `node --experimental-sea-config ./sea-config.json`;
+	const configPath = `./dist/sea-config-${target.asNodeString()}.json`;
+
+	const config = {
+		main: "./dist/partypooper.min.cjs",
+		output: `./dist/partypooper-${target.asNodeString()}.blob`,
+		disableExperimentalSEAWarning: true,
+		assets: {} as Record<string, string>
+	};
+
+	if (ffmpegMode === "static") {
+		const ffmpegPath = join(cache, `ffmpeg-${target.asNodeString()}${target.isWindows ? ".exe" : ""}`);
+		const ffmpegLicense = join(cache, "ffmpeg-LICENSE.txt");
+
+		config.assets.ffmpeg = ffmpegPath;
+		config.assets.ffmpegLicense = ffmpegLicense;
+	}
+
+	writeFileSync(configPath, JSON.stringify(config));
+
+	const blobCommand = `node --experimental-sea-config ${configPath}`;
 	execSync(blobCommand, { stdio: "inherit" });
 }
 
-async function getNode(target: string, targetNodeVersion: string) {
-	const fullTarget = `node-v${targetNodeVersion}-${target}`;
-	const fileExtension = target.includes("win") ? ".zip" : ".tar.gz";
-	const fileName = fullTarget + fileExtension;
-	const cachePath = join(homedir(), ".cache", "partypooper");
-	const executablePath = join(cachePath, fullTarget + (target.includes("win") ? ".exe" : ""));
+function ensureCache() {
+	if (!existsSync(cache)) mkdirSync(cache, {recursive: true});
+}
 
-	if (existsSync(executablePath)) {
-		console.log(`Using cached node: ${executablePath}`);
-		return executablePath;
-	} else {
-		console.log(`Downloading node: https://nodejs.org/dist/v${targetNodeVersion}/${fileName}`);
-		const url = `https://nodejs.org/dist/v${targetNodeVersion}/${fileName}`;
-		const response = await fetch(url);
-		if (!response.ok || !response.body) {
-			program.error(`Failed to download ${fullTarget}: ${response.status} ${response.statusText}`);
+function webStreamToNodeStream(webStream: ReadableStream) {
+	const reader = webStream.getReader();
+	return new Readable({
+		async read() {
+			const { done, value } = await reader.read();
+			if (done) {
+				this.push(null);
+			} else {
+				this.push(Buffer.from(value));
+			}
 		}
+	});
+}
 
-		const tempFilePath = join(tmpdir(), fileName);
+async function downloadAndExtract(url: string) {
+	const response = await fetch(url);
+	if (!response.ok || !response.body) {
+		program.error(`Failed to download ${url}: ${response.status} ${response.statusText}`);
+	}
+
+	const archive = join(tmpdir(), basename(url));
+
+	if (url.endsWith(".zip")) {
 		await pipeline(
 			response.body,
-			createWriteStream(tempFilePath)
+			createWriteStream(archive)
 		);
-
-		console.log(`Extracting node: ${fullTarget}`);
-		if (fileExtension === ".tar.gz") {
-			await extract({
-				file: tempFilePath,
-				cwd: tmpdir()
-			});
-		} else if (fileExtension === ".zip") {
-			await ExtractZip(tempFilePath, tmpdir());
-		} else {
-			program.error(`Unsupported file extension: ${fileExtension}`);
-		}
-
-		const unpackedDirectory = join(tmpdir(), fullTarget);
-
-		const nodeExecutable = target.includes("win")
-			? join(unpackedDirectory, "node.exe")
-			: join(unpackedDirectory, "bin", "node");
-		
-		if (!existsSync(nodeExecutable)) {
-			program.error(`Failed to find node executable: ${nodeExecutable}`);
-		}
-
-		if (!existsSync(cachePath)) {
-			mkdirSync(cachePath, {recursive: true});
-		}
-
-		renameSync(nodeExecutable, executablePath);
-
-		// unlinkSync(tempFilePath);
-		// rmSync(unpackedDirectory, {recursive: true, force: true});
-
-		console.log(`Cached node: ${executablePath}`);
-
-		return executablePath;
+		await ExtractZip(archive, tmpdir());
+	} else if (url.endsWith(".tar.gz")) {
+		await pipeline(
+			webStreamToNodeStream(response.body),
+			ExtractTar({cwd: tmpdir()})
+		);
+	} else if (url.endsWith(".tar.xz")) {
+		await pipeline(
+			webStreamToNodeStream(new XZ.XzReadableStream(response.body)),
+			ExtractTar({cwd: tmpdir()})
+		);
+	} else {
+		program.error(`Unsupported archive type: ${url}`);
 	}
+
+	return archive;
+}
+
+async function getNode(buildTarget: BuildTarget, targetVersion: NodeVersion) {
+	const archiveName = `node-${targetVersion}-${buildTarget.asNodeString()}`;
+	const nodePath = join(cache, archiveName + (buildTarget.isWindows ? ".exe" : ""));
+	
+	if (existsSync(nodePath)) {
+		console.log(`Using cached node executable: ${nodePath}`);
+		return nodePath;
+	}
+	
+	const url = `https://nodejs.org/dist/${targetVersion}/${archiveName}.${buildTarget.isWindows ? "zip" : "tar.gz"}`;
+	console.log(`Downloading node: ${url}`);
+	await downloadAndExtract(url);
+
+	const unpackedDirectory = join(tmpdir(), archiveName);
+	const nodeExecutable = buildTarget.isWindows
+		? join(unpackedDirectory, "node.exe")
+		: join(unpackedDirectory, "bin", "node");
+	
+	if (!existsSync(nodeExecutable)) {
+		program.error(`Failed to find node executable: ${nodeExecutable}`);
+	}
+
+	ensureCache();
+
+	copyFileSync(nodeExecutable, nodePath);
+
+	// unlinkSync(tempFilePath);
+	// rmSync(unpackedDirectory, {recursive: true, force: true});
+
+	console.log(`Cached node: ${nodePath}`);
+
+	return nodePath;
+}
+
+async function getFfmpeg(buildTarget: BuildTarget) {
+	const ffmpegTarget = `ffmpeg-${buildTarget.asNodeString()}`;
+	const ffmpegPath = join(cache, ffmpegTarget + (buildTarget.isWindows ? ".exe" : ""));
+
+	if (existsSync(ffmpegPath)) {
+		console.log(`Using cached ffmpeg executable: ${ffmpegPath}`);
+		return ffmpegPath;
+	}
+
+	const archiveName = `ffmpeg-master-latest-${buildTarget.asFfmpegString()}-lgpl`;
+	const url = `https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/${archiveName}.${buildTarget.isWindows ? "zip" : "tar.xz"}`;
+	console.log(`Downloading ffmpeg: ${url}`);
+	await downloadAndExtract(url);
+
+	const unpackedDirectory = join(tmpdir(), archiveName);
+	const ffmpegExecutable = buildTarget.isWindows
+		? join(unpackedDirectory, "bin", "ffmpeg.exe")
+		: join(unpackedDirectory, "bin", "ffmpeg");
+	const ffmpegLicense = join(unpackedDirectory, "LICENSE.txt");
+	
+	if (!existsSync(ffmpegExecutable)) {
+		program.error(`Failed to find ffmpeg executable: ${ffmpegExecutable}`);
+	}
+
+	ensureCache();
+
+	copyFileSync(ffmpegExecutable, ffmpegPath);
+	copyFileSync(ffmpegLicense, join(cache, "ffmpeg-LICENSE.txt"));
+
+	// unlinkSync(tempFilePath);
+	// rmSync(unpackedDirectory, {recursive: true, force: true});
+
+	console.log(`Cached ffmpeg: ${ffmpegPath}`);
+
+	return ffmpegPath;
 }
 
 program
 	.description("builds the project")
-	.argument("[targets...]", "Platform(s) to build for, e.g. 'linux-x64' or 'win-arm64'")
-	.option("--node-version <version>", "Node version to build for", process.version.slice(1))
-	.option("--debug", "Build in debug mode", false)
-	.action(async (targets, {nodeVersion: targetNodeVersion, debug: isDebug}) => {
-		build(isDebug);
+	.addArgument(
+		new Argument("[targets...]", "Platform(s) to build for, e.g. 'linux-x64' or 'win-arm64'")
+			.argParser((targetString, accumulator: BuildTarget[]) => {
+				try {
+					const target = new BuildTarget(targetString);
+					accumulator.push(target);
+					return accumulator;
+				} catch (error) {
+					program.error(`${error}`);
+				}
+			})
+			.default([] as BuildTarget[], `${process.platform}-${process.arch}`)
+	)
+	.addOption(
+		new Option("-n, --node-version <version>", "Node version to build for")
+			.argParser(versionString => {
+				try {
+					return new NodeVersion(versionString);
+				} catch (error) {
+					program.error(`${error}`);
+				}
+			})
+			.default(new NodeVersion(process.version), process.version)
+	)
+	.option("-d, --debug", "Build in debug mode", false)
+	.addOption(
+		new Option("-f, --ffmpeg <mode>", "Configure ffmpeg support. Static bundles ffmpeg into the binary. Shared uses the system ffmpeg. None disables ffmpeg support.")
+			.choices(["static", "shared", "none"] as const)
+			.default("static")
+	)
+	.action(
+		async (
+			targets,
+			{
+				nodeVersion: targetNodeVersion,
+				debug: isDebug,
+				ffmpeg: ffmpegModeUntyped
+			}
+		) => {
+			const ffmpegMode = ffmpegModeUntyped as FfmpegMode;
+			build(isDebug, ffmpegMode);
 
-		if (isDebug) {
-			console.log("Finished building debug");
-			return;
+			if (isDebug) {
+				console.log("Finished building debug");
+				return;
+			}
+
+			if (minimumNodeVersion.compare(targetNodeVersion) > 0) {
+				program.error(`Incompatible target build node version: ${targetNodeVersion}. Minimum required: ${minimumNodeVersion}`);
+			}
+
+			if (targets.length === 0) {
+				targets.push(new BuildTarget(process.platform + "-" + process.arch));
+			}
+			
+			for (const target of targets) {
+				if (ffmpegMode === "static") {
+					await getFfmpeg(target);
+				}
+
+				console.log(`Generating blob for ${target.asNodeString()}`);
+				generateBlob(target, ffmpegMode);
+			}
+
+			for (const target of targets) {
+				const executablePath = await getNode(target, targetNodeVersion);
+				const appName = `PartyPooper-${{static: "", shared: "ffmpeg-shared-", none: "ffmpeg-none-"}[ffmpegMode]}${target.asNodeString()}`;
+				const distExecutable = join("./dist", `${appName}${target.isWindows ? ".exe" : ""}`);
+				copyFileSync(executablePath, distExecutable);
+
+				console.log(`Injecting: ${distExecutable}`);
+
+				await inject(
+					distExecutable,
+					"NODE_SEA_BLOB",
+					readFileSync(`./dist/partypooper-${target.asNodeString()}.blob`),
+					{sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2"}
+				);
+
+				console.log(`Finished building ${appName}`);
+			}
 		}
-
-		if (targets.length === 0) {
-			program.error("No targets specified");
-		}
-
-		generateBlob();
-
-		targetNodeVersion ??= process.version;
-		const [major, minor] = targetNodeVersion.split(".").map(Number);
-		if (major < 20 || (major >= 20 && minor < 6)) {
-			program.error(`Unsupported target node version: ${targetNodeVersion}. The minimum supported version is 20.6.0`);
-		}
-
-		for (const target of targets) {
-			const executablePath = await getNode(target, targetNodeVersion);
-			const distExecutable = join("./dist", `PartyPooper-${target}${target.includes("win") ? ".exe" : ""}`);
-			copyFileSync(executablePath, distExecutable);
-
-			console.log(`Injecting: ${distExecutable}`);
-
-			await inject(
-				distExecutable,
-				"NODE_SEA_BLOB",
-				readFileSync("./dist/partypooper.blob"),
-				{sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2"}
-			);
-
-			console.log(`Finished building ${target}`);
-		}
-	})
+	)
 	.parse();
